778c58e812d112d2a481a063715740d7d27a7223
River Stanley <river.stanley173@gmail.com>
2026-04-01 11:53:20 -0400

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/mathsoc-icon.svg" sizes="any" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ClientLayout>{children}</ClientLayout>
       </body>
